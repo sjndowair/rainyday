1f2d96e9ce5a9251531b627774c9d03995f34754
sjndowair <wodnjsdl1125@naver.com>
2024-10-26 03:21:29 +0900

    fix: Layout 적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Loading from "../../components/loading";
 import { useEffect, useState } from "react";
+import Layout from "../../layout";
+// import { auth } from "../../constants/firebase.constants";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const isApiHandle = async () => {
+  //   const isApiHandle = async () => {
+  //     await auth.authStateReady();
+  //   };
+  useEffect(() => {
     setTimeout(() => {
       setIsLoading(true);
     }, 1000);
-  };
-  useEffect(() => {
-    isApiHandle();
   }, []);
 
   return (
     <>
-      {!isLoading ? <Loading /> : null}
-      <div>main home</div>
-      <Outlet />
+      <Layout>
+        {!isLoading ? <Loading /> : null}
+        <div>main home</div>
+        <Outlet />
+      </Layout>
     </>
   );
 };
