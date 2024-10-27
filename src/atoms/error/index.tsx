2fc2c33b158923bcafe7aa2ca94b87cce207aefc
sjndowair <wodnjsdl1125@naver.com>
2024-10-27 16:35:52 +0900

    feat: error component 추가

---
+++ src/atoms/error/index.tsx
@@ -0,0 +1,15 @@
+const Error = () => {
+  return (
+    <div
+      className="flex justify-center
+      items-center  bg-gradient-to-b
+       from-gray-700 to-gray-900 p-4 text-xl text-white max-w-[500px]
+        w-full absolute z-10 font-bold py-4 rounded-2xl right-[50%] translate-x-2/4 top-10
+        drop-shadow-2xl"
+    >
+      형식에 맞게 입력해주세요.
+    </div>
+  );
+};
+
+export default Error;
