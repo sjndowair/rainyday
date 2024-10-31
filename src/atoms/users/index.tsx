aa43a51ed9a0106b424af2a8ea65902134a92e20
sjndowair <wodnjsdl1125@naver.com>
2024-10-31 22:39:15 +0900

    feat: user atoms 추가

--- src/atoms/users/index.tsx
+++ src/atoms/users/index.tsx
@@ -1,24 +1,45 @@
+import { useState } from "react";
+import FeatureIcon from "../featureIcon";
 import { Heart, MessageCircle, Share2, Bookmark } from "lucide-react";
 
-const USER_ICON = [
-  { key: "Heart", icon: <Heart /> },
-  { key: "MessageCircle", icon: <MessageCircle /> },
-  { key: "share", icon: <Share2 /> },
-  { key: "BookMark", icon: <Bookmark /> },
-];
-
 const Users = () => {
+  const [isLikedPosts, setIsLikedPosts] = useState<Set<number>>(new Set());
+  const [isBookMark, setIsBookMark] = useState<Set<number>>(new Set());
+
+  const ICON_INUM = {
+    Heart: <Heart />,
+    MessageCircle: <MessageCircle />,
+    share: <Share2 />,
+    icoBookMarkn: <Bookmark />,
+  };
+
+  const isToggleLiked = (postId: number) => {
+    setIsLikedPosts((prev) => {
+      const newSet = new Set(prev);
+      newSet.has(postId) ? newSet.delete(postId) : newSet.add(postId);
+      return newSet;
+    });
+  };
+
+  const isToggleBookMark = (postId: number) => {
+    setIsBookMark((prev) => {
+      const newSet = new Set(prev);
+      newSet.has(postId) ? newSet.delete(postId) : newSet.add(postId);
+      return newSet;
+    });
+  };
+
   return (
-    <>
-      {USER_ICON.map((e, i) => (
-        <button
-          key={i}
-          className="flex items-center space-x-1 focus:outline-none group"
-        >
-          {e}
-        </button>
+    <div>
+      {Object.entries(ICON_INUM).map(([key, icon]) => (
+        <FeatureIcon
+          isBookMark={isBookMark}
+          key={key}
+          icon={icon}
+          onClick={() => isToggleBookMark}
+        />
       ))}
-    </>
+    </div>
   );
 };
 
