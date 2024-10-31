f18ad865ed4127038420f4112d4e4a47e35222a0
sjndowair <wodnjsdl1125@naver.com>
2024-11-01 00:02:26 +0900

    fix: 스켈레톤 ui추가를 위한 자식 컴포넌트 상태 추가

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,45 +1,26 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import InfoBox from "../../components/infoBox";
 import StoryBox from "../../components/storiesBox";
 import Post from "../../components/post";
-import {
-  Heart,
-  MessageCircle,
-  Share2,
-  Bookmark,
-  MoreHorizontal,
-} from "lucide-react";
-import { POST_DUMMY_DATA, STORY_DUMMY_DATA } from "../../dummy/dummy-data";
 
 const Profile = () => {
-  const [isLikedPosts, setIsLikedPosts] = useState<Set<number>>(new Set());
-  const [isBookMark, setIsBookMark] = useState<Set<number>>(new Set());
-
-  const isToggleLiked = (postId: number) => {
-    setIsLikedPosts((prev) => {
-      const newSet = new Set(prev);
-      newSet.has(postId) ? newSet.delete(postId) : newSet.add(postId);
-      return newSet;
-    });
-  };
-
-  const isToggleBookMark = (postId: number) => {
-    setIsBookMark((prev) => {
-      const newSet = new Set(prev);
-      newSet.has(postId) ? newSet.delete(postId) : newSet.add(postId);
-      return newSet;
-    });
-  };
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const isLoadingState = setTimeout(() => {
+      setIsLoading(true);
+    }, 3000);
+    return () => clearTimeout(isLoadingState);
+  }, []);
 
   return (
     <>
-      <main className="max-w-screen-xl mx-auto p-4">
-        <InfoBox />
-        <StoryBox />
-        <Post />
+      <main className="max-w-screen-xl mx-auto p-8 flex flex-col gap-10">
+        <InfoBox isLoading={isLoading} />
+        <StoryBox isLoading={isLoading} />
+        <Post isLoading={isLoading} />
       </main>
     </>
-    // </div>
   );
 };
 
