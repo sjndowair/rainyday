62e49649e0fbab32493e60888de6eeb18f03ab22
sjndowair <wodnjsdl1125@naver.com>
2024-11-02 17:38:04 +0900

    fix: 폴더명 변경 및 파일 명 변경
    loading => spinner

--- src/components/loading/spinner/index.tsx
+++ src/components/loading/spinner/index.tsx
@@ -1,19 +1,8 @@
 import { useEffect, useState } from "react";
-import RainEffect from "./Effect";
-import "./Effect/style.css";
 
 const Loading = () => {
-  const [isRaindrops, setIsRaindrops] = useState<number[]>([]);
-
-  useEffect(() => {
-    setIsRaindrops(Array.from({ length: 50 }, (_, i) => i));
-  }, []);
-
   return (
     <div className="relative flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-700 to-gray-900 overflow-hidden">
-      {isRaindrops.map((_, i) => (
-        <RainEffect key={i} delay={Math.random() * 1.5} />
-      ))}
       <div className="z-10 text-center">
         <div className="w-20 h-20 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin mb-4"></div>
         <p className="text-xl font-semibold text-blue-300">Loading...</p>
