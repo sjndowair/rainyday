25ae8fd6d0c81072f622ec6e93ed35d234bad5df
sjndowair <wodnjsdl1125@naver.com>
2024-11-02 17:38:23 +0900

    feat: post 컴포넌트 skelton ui추가

---
+++ src/loading/skeleton/skeletonPost/index.tsx
@@ -0,0 +1,26 @@
+import { POST_DUMMY_DATA } from "../../../dummy/dummy-data";
+
+const SkeletonPost = () => {
+  return (
+    <>
+      {POST_DUMMY_DATA.map((_, i) => (
+        <div
+          key={i}
+          className="bg-gray-800 bg-opacity-50 rounded-lg overflow-hidden backdrop-blur-sm"
+        >
+          <div className="p-4 flex items-center">
+            <div className="w-10 h-10 rounded-full bg-gray-600 animate-pulse mr-3"></div>
+            <div className="w-24 h-4 bg-gray-600 animate-pulse rounded"></div>
+          </div>
+          <div className="w-full h-48 bg-gray-600 animate-pulse"></div>
+          <div className="p-4">
+            <div className="w-full h-4 bg-gray-600 animate-pulse rounded mb-2"></div>
+            <div className="w-2/3 h-4 bg-gray-600 animate-pulse rounded"></div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SkeletonPost;
