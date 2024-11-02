9d0088772864f1adcd0d222e21395d543209cc56
sjndowair <wodnjsdl1125@naver.com>
2024-11-02 17:37:34 +0900

    feat: storyBox skeleton ui 추가

---
+++ src/loading/skeleton/skeletonStoryBox/index.tsx
@@ -0,0 +1,16 @@
+import { STORY_DUMMY_DATA } from "../../../dummy/dummy-data";
+
+const SkeletonStoryBox = () => {
+  return (
+    <>
+      {STORY_DUMMY_DATA.map((_, i) => (
+        <div key={i} className="flex gap-2 flex-col items-center">
+          <div className="w-16 h-16 rounded-full bg-gray-600 animate-pulse"></div>
+          <div className="w-14 h-3 mt-2 bg-gray-600 animate-pulse rounded"></div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default SkeletonStoryBox;
