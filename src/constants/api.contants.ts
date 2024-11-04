a80b40690f5414d074a73c9b1ac3b733a92cd51b
sjndowair <wodnjsdl1125@naver.com>
2024-11-05 00:23:38 +0900

    feat: api 기능 추가

---
+++ src/constants/api.contants.ts
@@ -0,0 +1,10 @@
+export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const BASE_URL = "https://api.openweathermap.org";
+
+export const REQUEST_INIT_OBJECT: RequestInit = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+  },
+};
