82b83eff0deac3dd4c6fb32ba38f3ec3cf7812c9
sjndowair <wodnjsdl1125@naver.com>
2024-11-05 21:54:44 +0900

    fix: isLoading 경로수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Loading from "../../components/loading";
+import Loading from "../../loading/spinner";
 import { useEffect, useState } from "react";
 import Layout from "../../layout";
-// import { auth } from "../../constants/firebase.constants";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  //   const isApiHandle = async () => {
-  //     await auth.authStateReady();
-  //   };
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(true);
@@ -18,9 +15,9 @@ const Home = () => {
   return (
     <>
       <Layout>
+        <Outlet />
         {!isLoading ? <Loading /> : null}
         <div>main home</div>
-        <Outlet />
       </Layout>
     </>
   );
