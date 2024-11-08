2f6fd39c85f1fe6b9f3b924cc270cdd9551947ad
sjndowair <wodnjsdl1125@naver.com>
2024-11-08 17:17:01 +0900

    fix: navigate path 추가

--- src/Molecule/navigation/index.tsx
+++ src/Molecule/navigation/index.tsx
@@ -1,28 +1,35 @@
 import { Home, Search, Bell, Mail, User } from "lucide-react";
+import {useNavigate} from "react-router-dom";
+import Noti from "../../components/noti"
 
-const isNavigationIcon = [
-  { key: "home", icon: <Home className="h-6 w-6" /> },
-  {
-    key: "search",
-    icon: <Search className="h-6 w-6" />,
-  },
-  { key: "bell", icon: <Bell className="h-6 w-6" /> },
-  { key: "mail", icon: <Mail className="h-6 w-6" /> },
-  { key: "user", icon: <User className="h-6 w-6" /> },
-];
+
 
 const Navigation = () => {
+
+const navigate = useNavigate();
+
+  const isNavigationIcon = [
+    { key: "home", icon: <Home className="h-6 w-6" />, path:""},
+    {
+      key: "search",
+      icon: <Search className="h-6 w-6"  />,
+    },
+    { key: "bell", icon: <Bell className="h-6 w-6" /> },
+    { key: "user", icon: <User className="h-6 w-6" />, path:"/mypage"},
+  ];
   return (
     <nav>
       <ul className="flex space-x-4">
         {isNavigationIcon.map((e, i) => (
           <li key={i}>
-            <button className="hover:text-blue-400 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-400 rounded-full p-1">
+            <button onClick={() => navigate(e?.path!)}
+             className="hover:text-blue-400 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-400 rounded-full p-1">
               {e.icon}
             </button>
           </li>
         ))}
       </ul>
+      <Noti />
     </nav>
   );
 };
--- src/atoms/logo/index.tsx
+++ src/atoms/logo/index.tsx
@@ -1,8 +1,15 @@
+import {useNavigate} from "react-router-dom";
+
 import { Umbrella } from "lucide-react";
 
+
+
 const Logo = () => {
+
+    const navigate = useNavigate();
+
   return (
-    <h1 className="text-2xl font-bold flex items-center">
+    <h1 onClick={() => navigate("/profile")} className="text-2xl font-bold flex items-center hover:cursor-pointer">
       <Umbrella className="mr-2" />
       RainyDaySocial
     </h1>
