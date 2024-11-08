adb16a2075ab5247bb3ddcda9b3f8069b9b5b631
sjndowair <wodnjsdl1125@naver.com>
2024-11-08 17:15:16 +0900

    feat: icon용 api호출 추가

--- src/apis/getWeatherApi.ts
+++ src/apis/getWeatherApi.ts
@@ -5,18 +5,30 @@ import {
 } from "../constants/api.contants";
 import { IWeatherApiProps } from "../types/weatherList";
 
+
 export const getWeatherList = async (
   lat: string | number,
   lon: string | number,
   reqInit: RequestInit = REQUEST_INIT_OBJECT
 ): Promise<IWeatherApiProps> => {
-  const res = await fetch(
-    `${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
-    reqInit
-  );
-  console.log(res);
-  if (!res.ok) {
-    throw new Error("api context 확인 필요");
+  try {
+    const response = await fetch(
+      `${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`,
+      reqInit
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch weather data:", error);
+    throw error;
   }
-  return await res.json();
 };
+
+interface IWeatherDataProps {
+  iconData: string
+}
+
+export const weatherIcon = ({iconData}: IWeatherDataProps) => {
+  return `http://openweathermap.org/img/wn/${iconData}@2x.png`
+}
