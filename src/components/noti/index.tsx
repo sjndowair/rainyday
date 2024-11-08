cd51d16146be84c918084d70fbaec2766cf48e7e
sjndowair <wodnjsdl1125@naver.com>
2024-11-08 17:16:09 +0900

    feat: 알림창 추가

---
+++ src/components/noti/index.tsx
@@ -0,0 +1,11 @@
+
+
+
+const Noti = () => {
+
+    return (<div className="rounded-2xl absolute w-[300px] max-w-full bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70 ">
+        노티
+    </div>)
+}
+
+export default Noti
