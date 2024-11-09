c395ce97487c5f7adb8e04786c03bce0e788261a
sjndowair <wodnjsdl1125@naver.com>
2024-11-09 17:48:32 +0900

    fix: api 프로토콜 수정

--- src/constants/api.contants.ts
+++ src/constants/api.contants.ts
@@ -1,6 +1,6 @@
 export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-export const BASE_URL = "https://api.openweathermap.org";
+export const BASE_URL = "http://api.openweathermap.org";
 
 export const REQUEST_INIT_OBJECT: RequestInit = {
   method: "GET",
