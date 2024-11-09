f0009e9eec78620a82dce80f8cf4faeead8d7872
sjndowair <wodnjsdl1125@naver.com>
2024-11-09 17:48:51 +0900

    fix: noti component style 수정

--- src/components/noti/index.tsx
+++ src/components/noti/index.tsx
@@ -1,11 +1,29 @@
+import React from "react";
+import useStore from "../../store";
+import {CircleX} from "lucide-react"
 
 
 
-const Noti = () => {
+interface INotiProps {
+    children?: React.ReactNode | string | JSX.Element
+}
 
-    return (<div className="rounded-2xl absolute w-[300px] max-w-full bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70 ">
-        노티
-    </div>)
+
+const Noti = ({children}:INotiProps) => {
+
+
+    const isCloseModal = useStore(state => state.isCloseModal)
+
+    return (
+        <>
+            <div className="rounded-2xl absolute top-[4.5rem] xl:right-[5%] lg:right-[2%] right-[2%] w-[300px] max-w-full bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70 ">
+        <button className="float-right pr-2 pt-2"  onClick={isCloseModal}><CircleX /></button>
+                <ul>
+                    {children}
+                </ul>
+    </div>
+        </>
+    )
 }
 
 export default Noti
