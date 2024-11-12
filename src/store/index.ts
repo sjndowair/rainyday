287eb06f4593378fc25e843e37aed9a05b214ca6
sjndowair <wodnjsdl1125@naver.com>
2024-11-12 21:22:23 +0900

    fix: store 위치 이동 및 기능 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 // store.js
 import {create} from "zustand/react";
-import firebase from "firebase/compat";
+
+import {IChatPageState} from "../types/create-chatPage";
 
 
 
@@ -12,6 +13,30 @@ const useStore = create((set:any) => ({
 
 }));
 
-export default useStore;
 
 
+
+const useChatStore = create<IChatPageState>((set) => ({
+    activeChat: null,
+    messages: {
+        'Alice': [
+            { id: 1, sender: 'Alice', content: "Hey, hows the weather there?", timestamp: '10:30 AM' },
+            { id: 2, sender: 'You', content: "Its raining cats and dogs!", timestamp: '10:32 AM' },
+        ],
+        'Bob': [
+            { id: 1, sender: 'Bob', content: 'Want to go for a walk in the rain?', timestamp: '11:15 AM' },
+            { id: 2, sender: 'You', content: 'Sure, let me grab my umbrella!', timestamp: '11:17 AM' },
+        ],
+    },
+
+
+    setActiveChat: (chat) => set({ activeChat: chat }),
+    addMessage: (chat, message) => set((state) => ({
+        messages: {
+            ...state.messages,
+            [chat]: [...(state.messages[chat] || []), message],
+        },
+    })),
+}))
+
+export {useStore, useChatStore};
