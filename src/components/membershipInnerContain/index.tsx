36c345810fb47ca572205fabc87993b5a4bf5d51
sjndowair <wodnjsdl1125@naver.com>
2024-11-14 20:40:14 +0900

    fix: 회원가입창 스타일 수정

--- src/components/membershipInnerContain/index.tsx
+++ src/components/membershipInnerContain/index.tsx
@@ -1,4 +1,4 @@
-import { ILayOutProps } from "../../types/create-membership";
+import {IButtonProps, ILayOutProps} from "../../types/create-membership";
 import { Umbrella } from "lucide-react";
 
 const MembershipInnerContain = ({ children }: ILayOutProps) => {
@@ -6,10 +6,7 @@ const MembershipInnerContain = ({ children }: ILayOutProps) => {
     <div className="relative  z-50 p-8">
       <div className="text-center mb-8">
         <Umbrella className="mx-auto h-12 w-12 text-blue-400" />
-        <h2 className="mt-4 text-3xl font-bold text-white">Rainy Day</h2>
-        <p className="mt-2 text-gray-400">
-          Hello! your created exclusive membership!
-        </p>
+        <h2 className="mt-4 text-3xl font-bold text-white pb-10">Rainy Day</h2>
         {children}
       </div>
     </div>
