cc72f2fc01e6ece49c7b13419e3aa66e0a240c0b
sjndowair <wodnjsdl1125@naver.com>
2024-11-16 19:14:02 +0900

    fix: membership page 회원가입 기능 구현
    fire base를 활용하여 기능 구현

--- src/pages/membership/index.tsx
+++ src/pages/membership/index.tsx
@@ -1,37 +1,87 @@
-import { useState } from "react";
+
+import React from 'react';
+import {useState} from "react";
 
 //fire base
-import {auth, db, USER_COLLECTION} from "../../constants/firebase-contants";
+import "firebase/firestore"
+import {auth, db} from "../../constants/firebase-contants";
+import {doc, setDoc} from "firebase/firestore"
+import {createUserWithEmailAndPassword} from "firebase/auth"
+
+
+//Router
+import {useNavigate} from "react-router-dom";
 
 //atoms
 import Layout from "../../layout";
 import Form from "../../atoms/form";
 import Button from "../../atoms/button";
 import InputBox from "../../components/inputBox";
-import RadioBox from "../../components/radioBox";
 import Error from "../../atoms/error";
 import MembershipInnerContain from "../../components/membershipInnerContain";
 
 
+
 const Membership = () => {
+
+  const navigate = useNavigate();
+
+  const [isFormData, setIsFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    address: "",
+  })
+
   const [isError, setIsError] = useState<boolean>(false);
 
-  const isErrorHandle = () => {
-    if (InputBox.length === 0) setIsError(true);
+   const isHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setIsFormData((pre) => ({
+      ...pre, [name]:value
+    }))
+
+  }
+
+
+  console.log("isFormData",isFormData);
+
+  const isHandleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try{
+      const userCredential = await (createUserWithEmailAndPassword(auth, isFormData.email, isFormData.password))
+      const user = userCredential.user
+
+      console.log("isUserData",user)
+
+      await setDoc(doc(db, "users", user.uid), {
+          name: isFormData.name,
+          email: isFormData.email,
+          address: isFormData.address,
+      });
+      console.log("user 등록이 완료되었습니다.  '다음에 할건 회원가입 완료 시 로그인 페이지로 이동'")
+        navigate("/login")
+    } catch(e) {
+      if(e) {
+        console.log("등록오류:", e)
+       }
+    }
+
   };
 
   return (
     <Layout>
-      {isError ? <Error /> : null}
+      {isError && <Error /> }
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 text-gray-200">
         <div className="relative max-w-md w-full bg-gray-800 rounded-lg shadow-xl ">
-          <div className="absolute inset-0 opacity-20" />
+          <div className="absolute inset-0 opacity-20 flex flex-col" />
           <MembershipInnerContain>
-            <Form>
+            <Form isHandleSubmit={isHandleSubmit}>
               <InputBox
-                value=""
+                value={isFormData.name}
                 label="Name"
-                htmlFor="name"
+
+                isHandleChange={isHandleChange}
                 type="text"
                 id="name"
                 name="name"
@@ -39,9 +89,9 @@ const Membership = () => {
                 placeholder="yourName"
               />
               <InputBox
-                value=""
+                value={isFormData.email}
                 label="email"
-                htmlFor="email"
+                isHandleChange={isHandleChange}
                 type="email"
                 id="email"
                 placeholder="your email"
@@ -49,9 +99,9 @@ const Membership = () => {
                 required
               />
               <InputBox
-                  value=""
+                  value={isFormData.password}
                   label="password"
-                  htmlFor="password"
+                  isHandleChange={isHandleChange}
                   type="password"
                   id="password"
                   placeholder="your password"
@@ -59,34 +109,14 @@ const Membership = () => {
                   required
               />
               <InputBox
-                  value=""
+                  value={isFormData.address}
                   label="address"
-                  htmlFor="address"
+                  isHandleChange={isHandleChange}
                   type="text"
-                  id="address"
                   placeholder="your address"
                   name="address"
                   required
               />
-              <div>
-                <label className=" text-sm font-medium text-gray-300 mb-2 flex justify-start">
-                  your sex
-                </label>
-                <div className="flex space-x-4">
-                  <RadioBox
-                    label="your sex"
-                    type="radio"
-                    name="sex"
-                    value="men"
-                  />
-                  <RadioBox
-                    label="your sex"
-                    type="radio"
-                    name="sex"
-                    value="women"
-                  />
-                </div>
-              </div>
               <Button login={"Click Me!"} />
             </Form>
           </MembershipInnerContain>
