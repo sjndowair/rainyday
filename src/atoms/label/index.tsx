e8665a2a71c7f4a9dc4c3995e58682fb1ec78b6d
sjndowair <wodnjsdl1125@naver.com>
2024-11-16 19:12:04 +0900

    fix: label 스타일 수정

--- src/atoms/label/index.tsx
+++ src/atoms/label/index.tsx
@@ -2,7 +2,7 @@ import { ILabelProps } from "../../types/create-membership";
 
 const Label = ({ htmlFor, className, children }: ILabelProps) => {
   return (
-    <label htmlFor={htmlFor} className={className}>
+    <label htmlFor={htmlFor} className="flex flex-col items-start">
       {children}
     </label>
   );
