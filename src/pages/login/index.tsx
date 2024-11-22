3151c60b5b31e4f332438e514f3da314e639b0f0
sjndowair <wodnjsdl1125@naver.com>
2024-11-22 23:13:47 +0900

    feat: logout 기능 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-import Layout from "../../layout";
 import Form from "../../atoms/form";
 import Button from "../../atoms/button";
 import InputBox from "../../components/inputBox";
-import Error from "../../atoms/error";
 import MembershipInnerContain from "../../components/membershipInnerContain";
 import {useState} from "react";
+import { signOut} from "firebase/auth";
+import {auth} from "../../constants/firebase-contants";
+import {useNavigate} from "react-router-dom";
 
 
-const LOGIN = "Login Click"
 
 const Login = ()=> {
+
+    const navigate = useNavigate();
+
+
+const onClickLogOut = async  () => {
+
+        const logOut =  signOut(auth);
+try {
+    await  logOut
+    navigate("/membership")
+    console.log(auth);
+
+} catch (e){
+    console.error(e);
+}
+
+
+
+}
 
     const [isInputData, setIsInputData] = useState({
         email: "",
@@ -27,10 +46,8 @@ const Login = ()=> {
     }
 
   return (
-      <Layout>
-
+      <div>
         <div className="min-h-screen flex items-center  justify-center bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 text-gray-200">
-
           <div className="relative max-w-md w-full bg-gray-800 rounded-lg shadow-xl ">
             <div className="absolute inset-0 opacity-20 " />
             <MembershipInnerContain>
@@ -58,15 +75,17 @@ const Login = ()=> {
                     required
                 />
                 <div className="pb-5"/>
-                <Button login={LOGIN} />
+                <Button  />
+
               </Form>
+                <Button onClickLogOut={onClickLogOut}/>
             </MembershipInnerContain>
 
           </div>
 
         </div>
-        <Error />
-      </Layout>
+
+      </div>
   );
 }
 
