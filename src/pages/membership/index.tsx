0b4c84a34d0813bf04ae7419635cf5de879b72fc
sjndowair <wodnjsdl1125@naver.com>
2024-11-22 23:13:31 +0900

    fix: layout 컴포넌트 div로 수정

--- src/pages/membership/index.tsx
+++ src/pages/membership/index.tsx
@@ -6,27 +6,24 @@ import {useState, useEffect} from "react";
 import "firebase/firestore"
 
 import {auth, db, USER_COLLECTION} from "../../constants/firebase-contants";
-import {doc, setDoc} from "firebase/firestore"
+import {doc} from "firebase/firestore"
 import {createUserWithEmailAndPassword} from "firebase/auth"
 
 //Router
 import {useNavigate} from "react-router-dom";
 
 //atoms
-import Layout from "../../layout";
-import Form from "../../atoms/form";
+
+
 import Button from "../../atoms/button";
 import InputBox from "../../components/inputBox";
-import Error from "../../atoms/error";
 import MembershipInnerContain from "../../components/membershipInnerContain";
 
 
 
 const Membership = () => {
 
-  const navigate = useNavigate();
-
-
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isFormData, setIsFormData] = useState({
     name: "",
     email: "",
@@ -34,10 +31,7 @@ const Membership = () => {
     address: "",
   })
 
-
-
-  const [isError, setIsError] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
 
    const isHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget;
@@ -47,8 +41,6 @@ const Membership = () => {
 
   }
 
-
-
   const isHandleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
@@ -57,23 +49,17 @@ const Membership = () => {
       const { user } = await createUserWithEmailAndPassword(auth, isFormData.email, isFormData.password)
         const useDoc = doc(USER_COLLECTION, user.uid)
       const { uid } = user
-
-
+      console.log(uid)
 
       navigate("/login")
-      console.log("user 등록이 완료되었습니다.  '다음에 할건 회원가입 완료 시 로그인 페이지로 이동'")
-      console.log(uid)
     } catch(e) {
-      if(e) {
-        console.log("등록오류:", e)
-       }
+      console.log(e);
     }
 
   };
 
   return (
-    <Layout>
-      {isError && <Error /> }
+    <div>
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 text-gray-200">
         <div className="relative max-w-md w-full bg-gray-800 rounded-lg shadow-xl ">
           <div className="absolute inset-0 opacity-20 flex flex-col" />
@@ -119,12 +105,12 @@ const Membership = () => {
                   name="address"
                   required
               />
-              <Button login={"Click Me!"} />
+              <Button isLoading={isLoading} login={"Click Me!"} />
             </form>
           </MembershipInnerContain>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 }
 export default Membership;
