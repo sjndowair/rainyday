0b99fcf027b8c89774fd61dad80fb15c08ef6ad2
sjndowair <wodnjsdl1125@naver.com>
2024-11-22 23:12:28 +0900

    fix: 로그인 상태 및 로그아웃 기능 추가
    회원가입 진행 전 후 컴포넌트의 텍스트 변경 및 로그아웃 기능 추가

--- src/atoms/button/index.tsx
+++ src/atoms/button/index.tsx
@@ -1,10 +1,15 @@
-const Button = () => {
+import {IButtonProps} from "../../types/create-membership";
+
+
+const Button = ({isLoading, onClickLogOut}:IButtonProps) => {
+
   return (
     <button
+        onClick={onClickLogOut}
       type="submit"
-      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+      className=" w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 "
     >
-      Join Now
+      {!isLoading ? "join now!" : "now Loading..."}
     </button>
   );
 };
