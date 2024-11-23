4b04688301308a8e132da424bb327fc8c489def1
sjndowair <wodnjsdl1125@naver.com>
2024-11-23 17:11:21 +0900

    feat: reaact routerdom 사용을 위한 dom 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./routes/index";
 import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import "tailwindcss/tailwind.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const QueryProvider = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={QueryProvider}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
