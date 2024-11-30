deb5ed5af0f2ed0e4c382858ed02b42f395f3f8e
sjndowair <wodnjsdl1125@naver.com>
2024-11-30 17:50:09 +0900

    feat: error page 타입 추가

--- src/atoms/error/index.tsx
+++ src/atoms/error/index.tsx
@@ -1,11 +1,19 @@
-const Error = () => {
+
+
+interface IErrorBoxProps {
+  isEmailExists?: boolean;
+}
+
+const Error = ({isEmailExists}:IErrorBoxProps) => {
   return (
     <div
-      className="flex justify-center
-      items-center  bg-gradient-to-b
-       from-gray-700 to-gray-900 p-4 text-xl text-white max-w-[500px]
+      className={`
+        flex justify-center
+        items-center  bg-gradient-to-b
+        from-gray-700 to-gray-900 p-4 text-xl text-white max-w-[500px]
         w-full absolute z-10 font-bold py-4 rounded-2xl right-[50%] translate-x-2/4 top-[8rem]
-        drop-shadow-2xl"
+        drop-shadow-2xl ${isEmailExists ? 'opacity-100' : 'opacity-0'} transition-all duration-300`
+      }
     >
       형식에 맞게 입력해주세요.
     </div>
