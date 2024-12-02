04339f13298706a37b9badf4dfee08a9b2090ba4
sjndowair <wodnjsdl1125@naver.com>
2024-12-03 00:46:48 +0900

    fix: chartButton 매커니즘 변경

--- src/atoms/chartButton/index.tsx
+++ src/atoms/chartButton/index.tsx
@@ -4,31 +4,25 @@ import {useThemeStore} from "../../store";
 
 interface IChartButtonProps {
     value?: string;
+    type?: string;
+    key?: string | number;
     isActiveChart?: string | [];
     onClickActiveChart?: (e: string) => void;
+    setIsActiveChart?: any;
 }
 
 const CHART_DATA = ["candlestick", "line", "donut"]
 
-const ChartButton = ({value, isActiveChart, onClickActiveChart}: IChartButtonProps) =>  {
+const ChartButton = ({value ,type ,setIsActiveChart, isActiveChart}: IChartButtonProps) =>  {
 
-    // console.log(onClickActiveChart)
     const {isDarkMode} = useThemeStore();
-
-    if(!onClickActiveChart) return null;
-
-
-
 
     return (
         <>
-            {CHART_DATA.map((e, i) => (
-                <button onClick={() => onClickActiveChart(e)}
-                        key={i}
-                        className={`px-3 py-1 rounded ${isActiveChart === e ? (isDarkMode ? 'bg-blue-600' : "bg-purple-500") : 'bg-gray-700'}`}
-                >{e}</button>
+                <button onClick={() => setIsActiveChart(type)}
 
-            ))}
+                        className={`px-3 py-1 rounded ${isDarkMode ?  'bg-blue-600' : "bg-purple-500"} bg-opacity-70 hover:bg-opacity-90`}
+                >{type?.charAt(0).toUpperCase() + type!.slice(1)}</button>
         </>
     )
 
