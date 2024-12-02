16cef17840177349d66df0207263d26d38332096
sjndowair <wodnjsdl1125@naver.com>
2024-12-03 00:49:58 +0900

    fix: 로그인 및 회원가입 컴포넌트추가

--- src/components/membershipInnerContain/index.tsx
+++ src/components/membershipInnerContain/index.tsx
@@ -5,8 +5,8 @@ const MembershipInnerContain = ({ children }: ILayOutProps) => {
   return (
     <div className="relative  z-50 p-8">
       <div className="text-center mb-8">
-        <Umbrella className="mx-auto h-12 w-12 text-blue-400" />
-        <h2 className="mt-4 text-3xl font-bold text-white pb-10">Rainy Day</h2>
+
+        <h2 className="mt-4 text-3xl font-bold text-white pb-10">LOGIN</h2>
         {children}
       </div>
     </div>
