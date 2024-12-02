ee98f38688a27a68c2cd19fcbe2d9a1447ad94e4
sjndowair <wodnjsdl1125@naver.com>
2024-12-03 00:49:19 +0900

    fix: infobox style 수정

--- src/components/infoBox/index.tsx
+++ src/components/infoBox/index.tsx
@@ -5,12 +5,14 @@ import SkeletonInfoBox from "../../loading/skeleton/skeletonInfoBox";
 import { getWeatherList } from "../../apis/getWeatherApi";
 import {weatherIcon} from "../../apis/getWeatherApi";
 import {weatherGenres} from "../../utils/weather.Util";
-
+import {useThemeStore} from "../../store";
 
 
 const InfoBox = () => {
   const [isLatitube, setIsLatitube] = useState<number >();
   const [isLongtitube, setIsLongtitube] = useState<number>();
+
+  const { isDarkMode } = useThemeStore()
 
   const { data, isLoading } = useQuery({
     queryKey: ["weatherData", isLatitube, isLongtitube],
@@ -30,16 +32,16 @@ const InfoBox = () => {
   const isIconUrl = () => {
     if(iconData) return weatherIcon({iconData});
   }
-
+console.log(isIconUrl())
 
   return (
     <>
       {isLoading ? (
         <SkeletonInfoBox />
       ) : (
-        <div className="mb-6 bg-blue-900 bg-opacity-30 rounded-lg p-4 flex items-center justify-between h-[4rem]">
+        <div className={`mb-6 ${isDarkMode ? "bg-blue-900 bg-opacity-30" : "bg-purple-400 bg-opacity-60 text-white"} rounded-lg p-4 flex items-center justify-between h-[4rem]`}>
           <div className="flex gap-2 items-center">
-            <Cloud className="h-8 w-8 mr-2 text-blue-300" />
+            <Cloud className={`h-8 w-8 mr-2  ${isDarkMode ? "text-blue-300" : "text-white"}`} />
             <img src={isIconUrl()} />
             <span className="text-lg font-semibold">
              {data?.name}
