e2fa8028d2b6826be8104aba704d3319821b879b
sjndowair <wodnjsdl1125@naver.com>
2024-12-03 00:47:57 +0900

    fix: 회원가입 기능 수정

--- src/pages/membership/index.tsx
+++ src/pages/membership/index.tsx
@@ -1,10 +1,10 @@
 //React
-import React, {useId} from 'react';
+import React, {useId, useRef} from 'react';
 import {useState} from "react";
 
 //fire base
 import "firebase/firestore"
-import {auth, db, USER_COLLECTION} from "../../constants/firebase-contants";
+import {auth, db, checkIfEmailExists} from "../../constants/firebase-contants";
 import {createUserWithEmailAndPassword ,setPersistence,browserLocalPersistence } from "firebase/auth"
 
 //Router
@@ -15,17 +15,23 @@ import Button from "../../atoms/button";
 import InputBox from "../../components/inputBox";
 import MembershipInnerContain from "../../components/membershipInnerContain";
 import Spinner from "../../loading/spinner";
+import Error from "../../atoms/error";
+import Theme from "../../components/theme";
+import CenterBox from "../../components/centerBox";
 
 
 const Membership = () => {
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isEmailExists, setIsEmailExists] = useState<boolean>(false);
   const [isFormData, setIsFormData] = useState({
     name: "",
     email: "",
     password: "",
     address: "",
   })
+
+
 
   const navigate = useNavigate();
 
@@ -38,16 +44,29 @@ const Membership = () => {
   }
 
   const isHandleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+
     e.preventDefault();
-    setIsLoading(true);
+
+    const emailExists= await checkIfEmailExists(isFormData.email);
+
+    if(emailExists) {
+      return;
+    } else {
+        setIsEmailExists(true)
+        setTimeout(() => {
+            setIsEmailExists(false);
+        }, 1000)
+    }
+console.log("에러박스 상태값", isEmailExists)
 
     try{
       const { user } = await createUserWithEmailAndPassword(auth, isFormData.email, isFormData.password)
       setPersistence(auth, browserLocalPersistence)
-        // const useDoc = doc(USER_COLLECTION, user.uid)
+      setIsLoading(true);
       const { uid } = user
       console.log(uid)
      console.log(user)
+
       navigate("/home")
     } catch(e) {
       console.log(e);
@@ -58,59 +77,64 @@ const Membership = () => {
   };
 
   return (
-    <div>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 text-gray-200">
-        <div className="relative max-w-md w-full bg-gray-800 rounded-lg shadow-xl ">
-          <div className="absolute inset-0 opacity-20 flex flex-col" />
-          <MembershipInnerContain>
-            <form className="space-y-6" onSubmit={isHandleSubmit}>
-              <InputBox
-                value={isFormData.name}
-                label="Name"
+      <Theme>
+          <div
+              className="min-h-screen  text-white p-8 flex justify-center items-center ">
+              <CenterBox >
+                  <div className="absolute inset-0 opacity-20 flex flex-col"/>
+                  <MembershipInnerContain>
+                      {isEmailExists && <Error isEmailExists={isEmailExists}/>}
 
-                isHandleChange={isHandleChange}
-                type="text"
-                id="name"
-                name="name"
-                required
-                placeholder="yourName"
-              />
-              <InputBox
-                value={isFormData.email}
-                label="email"
-                isHandleChange={isHandleChange}
-                type="email"
-                id="email"
-                placeholder="your email"
-                name="email"
-                required
-              />
-              <InputBox
-                  value={isFormData.password}
-                  label="password"
-                  isHandleChange={isHandleChange}
-                  type="password"
-                  id="password"
-                  placeholder="your password"
-                  name="password"
-                  required
-              />
-              <InputBox
-                  value={isFormData.address}
-                  label="address"
-                  isHandleChange={isHandleChange}
-                  type="text"
-                  placeholder="your address"
-                  name="address"
-                  required
-              />
-              <Button isLoading={isLoading} login={"Click Me!"} />
-            </form>
-            <div onClick={() => navigate("login")} className="pt-2 cursor-pointer hover: underline-offset-1">이미 회원이신가요?</div>
-          </MembershipInnerContain>
-        </div>
-      </div>
-    </div>
+                      <form className="space-y-6" onSubmit={isHandleSubmit}>
+                          <InputBox
+                              value={isFormData.name}
+                              label="Name"
+
+                              isHandleChange={isHandleChange}
+                              type="text"
+                              id="name"
+                              name="name"
+                              required
+                              placeholder="yourName"
+                          />
+                          <InputBox
+                              value={isFormData.email}
+                              label="email"
+                              isHandleChange={isHandleChange}
+                              type="email"
+                              id="email"
+                              placeholder="your email"
+                              name="email"
+                              required
+                          />
+                          <InputBox
+                              value={isFormData.password}
+                              label="password"
+                              isHandleChange={isHandleChange}
+                              type="password"
+                              id="password"
+                              placeholder="your password"
+                              name="password"
+                              required
+                          />
+                          <InputBox
+                              value={isFormData.address}
+                              label="address"
+                              isHandleChange={isHandleChange}
+                              type="text"
+                              placeholder="your address"
+                              name="address"
+                              required
+                          />
+                          <Button isLoading={isLoading} login={"Click Me!"}>회원가입</Button>
+                      </form>
+                      <div onClick={() => navigate("login")}
+                           className="pt-2 cursor-pointer pt-6 hover: underline-offset-1">이미 회원이신가요?
+                      </div>
+                  </MembershipInnerContain>
+              </CenterBox>
+          </div>
+      </Theme>
   );
 }
 export default Membership;
