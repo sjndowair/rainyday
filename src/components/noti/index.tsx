94c27b0ba75850cc7da7537ab97ea4e57f51a879
sjndowair <wodnjsdl1125@naver.com>
2024-12-02 23:38:59 +0900

    feat: noti component 스타일 수정

--- src/components/noti/index.tsx
+++ src/components/noti/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useStore} from "../../store";
+import {useChatStore, useStore, useThemeStore} from "../../store";
 import {CircleX} from "lucide-react"
 
 
@@ -12,11 +12,16 @@ interface INotiProps {
 const Noti = ({children}:INotiProps) => {
 
 
+    const {isDarkMode} = useThemeStore();
+
     const isCloseModal = useStore(state => state.isCloseModal)
 
     return (
         <>
-            <div className="z-[9999] rounded-2xl absolute top-[4.5rem] xl:right-[5%] lg:right-[2%] right-[2%] w-[300px] max-w-full bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70 ">
+            <div className={`z-[99999] rounded-2xl absolute top-[4.5rem] xl:right-[5%] lg:right-[2%] right-[2%] w-[300px] max-w-full 
+            ${isDarkMode ? " bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70"
+                : "bg-purple-600 bg-opacity-20 h-[20rem] hover:bg-opacity-40 focus:bg-opacity-50 text-white"}
+            `}>
         <button className="float-right pr-2 pt-2"  onClick={isCloseModal}><CircleX /></button>
                 <ul>
                     {children}
