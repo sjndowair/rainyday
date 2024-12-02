578a051d28eab46d52ed5d2cabf5d3a87ded1aec
sjndowair <wodnjsdl1125@naver.com>
2024-12-03 00:47:14 +0900

    fix: 당장 사용하지않을 기능 주석처리
    비오는 모션 및 눈오는 모션

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -17,22 +17,22 @@ const Layout = ({ children }: ILayout) => {
 
   const isModalState = useStore(state => state.modalState);
   const isCloseModal = useStore(state => state.isCloseModal);
-  // const [onClickToggleTheme, se]
+
   const {toggleTheme,isDarkMode} = useThemeStore();
 
-
-  useEffect(() => {
-    const isCreateRainDropEffect = () => {
-      const isRainDrop = document.createElement("div");
-      isRainDrop.classList.add("rainDrop");
-      isRainDrop.style.left = `${Math.random() * 100}%`;
-      isRainDrop.style.animationDuration = `${Math.random() * 1 + 0.5}s`;
-      document.getElementById("rain-container")?.appendChild(isRainDrop);
-      setTimeout(() => isRainDrop.remove(), 2000);
-    };
-    const isRemoveRainDropEffect = setInterval(isCreateRainDropEffect, 100);
-    return () => clearInterval(isRemoveRainDropEffect);
-  }, []);
+  //
+  // useEffect(() => {
+  //   const isCreateRainDropEffect = () => {
+  //     const isRainDrop = document.createElement("div");
+  //     isRainDrop.classList.add("rainDrop");
+  //     isRainDrop.style.left = `${Math.random() * 100}%`;
+  //     isRainDrop.style.animationDuration = `${Math.random() * 1 + 0.5}s`;
+  //     document.getElementById("rain-container")?.appendChild(isRainDrop);
+  //     setTimeout(() => isRainDrop.remove(), 2000);
+  //   };
+  //   const isRemoveRainDropEffect = setInterval(isCreateRainDropEffect, 100);
+  //   return () => clearInterval(isRemoveRainDropEffect);
+  // }, []);
   return (
            <Member>
           <Header />
