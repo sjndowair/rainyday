ea292c909f35257970b4e1b42298c9a1c134907e
sjndowair <wodnjsdl1125@naver.com>
2024-12-09 21:03:59 +0900

    Revert "fix: chat page 수정"
    This reverts commit a0c6a674b03515036d93b8bf4e44ec35feeb5336.
    
    # Conflicts:
    #	src/pages/chat/index.tsx

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -3,17 +3,19 @@ import { useState } from 'react'
 import {useChatStore} from "../../store";
 
 import { auth, db } from "../../constants/firebase-contants";
-import {getDatabase, ref, push, onChildAdded} from "firebase/database"
+// import {getDatabase, ref, push, onChildAdded} from "firebase/database"
 import Layout from "../../layout";
 import ChatArea from "../../components/ChatArea";
 import UserSidebar from "../../components/sidebar";
 import Theme from "../../components/theme";
 
 
+
  const MainPage =() => {
     const { activeChat, message, setActiveChat, addMessage } = useChatStore()
 
     const [newMessage, setNewMessage] = useState('')
+
 
 
     const handleSendMessage = () => {
@@ -26,6 +28,48 @@ import Theme from "../../components/theme";
             })
             setNewMessage('')
         }
+
+ const MainPage =() => {
+
+     const [messages, setMessages] = useState<ISendMessageProps[]>([]);
+     const [newMessage, setNewMessage] = useState('');
+
+     const sendMessage = async ({userId, message}:ISendMessageProps) => {
+
+         try {
+        await addDoc(collection(db, "messages"), {
+            userId,
+            text: message,
+            createdAt: new Date().toISOString(),
+        });
+         } catch (e) {
+             console.log("this message Error:", e)
+         }
+     }
+
+     const subscribeToMessages = (callback:Dispatch<SetStateAction<ISendMessageProps[]>>) => {
+        const q = query(collection(db, "message"),
+            orderBy("createdAt"));
+        return onSnapshot(q, (querySnapshot) => {
+            const messages = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                    ...doc.data()
+            }));
+            callback(messages)
+        })
+     }
+
+     useEffect(() => {
+         const unsubscribe = subscribeToMessages(setMessages);
+         return () => unsubscribe()
+     }, []);
+
+    const onClickSendMessage = () => {
+         if(newMessage.trim()){
+            sendMessage({userId: "currentUserId", message: newMessage})
+             setNewMessage("");
+         }
+
     }
 
     return (
@@ -34,7 +78,12 @@ import Theme from "../../components/theme";
             <div className={`flex`}>
             <div id="rain-container" className="fixed inset-0 pointer-events-none" />
            <UserSidebar />
+
           <ChatArea />
+
+          <ChatArea messages={messages} setMessages={setMessages}
+                    newMessage={newMessage} setNewMessage={setNewMessage} onClickSendMessage={onClickSendMessage} />
+
             </div>
         </Theme>
         </Layout>
