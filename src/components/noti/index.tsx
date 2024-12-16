a735008e4c0b50983fe1d60aa2048ddc76e378ac
sjndowair <wodnjsdl1125@naver.com>
2024-12-16 21:34:10 +0900

    fix: 로그인 상태에 따른 모달 상태 변경

--- src/components/noti/index.tsx
+++ src/components/noti/index.tsx
@@ -1,32 +1,111 @@
-import React from "react";
-import {useChatStore, useStore, useThemeStore} from "../../store";
-import {CircleX} from "lucide-react"
+import React, {useEffect, useState} from "react";
+import { useStore, useThemeStore} from "../../store";
+import {auth} from "../../constants/firebase-contants"
+import {GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth"
+import {User, Key, GlobeLock, Store, LogOut} from "lucide-react"
+import {useNavigate} from "react-router-dom";
 
 
 
 interface INotiProps {
-    children?: React.ReactNode | string | JSX.Element
+    children?: React.ReactNode | string | JSX.Element;
 }
 
+const USER = auth.currentUser;
 
 const Noti = ({children}:INotiProps) => {
 
+const [isUserState, setIsUserState] = useState(USER);
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, ((currentUser) => {
+             setIsUserState(currentUser);
+        }))
+        return () => unsubscribe()
+    }, []);
+
+
+
+    const isOnClickLogOut = async () => {
+        const logOut = signOut(auth);
+        try {
+            await logOut
+            navigate("/")
+            console.log("logOut:", logOut)
+        }catch (error) {
+            console.log("logOut error:", error);
+        }
+    }
+
+    const isOnClickGoogleSignIn = async () => {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(auth, provider);
+            console.log(result);
+            navigate("/");
+            console.log("USER signed in:", result.user);
+        } catch (error) {
+            console.log("USER signed in Error:", error);
+        }
+    }
+
+    const navigate = useNavigate();
+
+    const onClickUserItem = (key: string) => {
+        switch (key){
+            case "로그인":
+                navigate("/login");
+                break;
+            case "회원가입" :
+                navigate("/membership");
+                break;
+            case "마이페이지" :
+                navigate("/mypage");
+                break
+            case "구글로 로그인하기":
+                isOnClickGoogleSignIn();
+                break
+            case "로그아웃":
+                isOnClickLogOut();
+                break
+        }
+
+    }
+
+    const SIGN_IN = [
+    {key: "마이페이지", icon: <User />},
+        {key: "로그아웃", icon:<LogOut /> }
+]
+
+
+    const NO_SIGN_USER = [
+        {key: "로그인" , icon: <Key />},
+        {key: "회원가입", icon: <Store />},
+        {key: "구글로 로그인하기", icon: <GlobeLock />}
+    ]
     const {isDarkMode} = useThemeStore();
 
     const isCloseModal = useStore(state => state.isCloseModal)
 
     return (
         <>
-            <div className={`z-[99999] rounded-2xl absolute top-[4.5rem] xl:right-[5%] lg:right-[2%] right-[2%] w-[300px] max-w-full 
-            ${isDarkMode ? " bg-gray-800 bg-opacity-50 h-[20rem] hover:bg-opacity-70 focus:bg-opacity-70"
-                : "bg-purple-600 bg-opacity-20 h-[20rem] hover:bg-opacity-40 focus:bg-opacity-50 text-white"}
+            <div className={`z-[99999] rounded-2xl absolute top-[4.5rem] xl:right-[1%] lg:right-[2%] right-[2%] w-[300px] max-w-full 
+            ${isDarkMode ? " bg-gray-800 bg-opacity-50  hover:bg-opacity-70 focus:bg-opacity-70"
+                : "bg-gray-100 text-black shadow-purple-500 shadow-xl"}
             `}>
-        <button className="float-right pr-2 pt-2"  onClick={isCloseModal}><CircleX /></button>
-                <ul>
-                    {children}
+
+                <ul className={`flex flex-col py-6 `}>
+                    {!isUserState ?  (NO_SIGN_USER.map((e, i) => (
+                            <li className={`flex gap-3 mx-5 pl-2 transition-all duration-200 ease-in 
+                        hover:cursor-pointer hover:bg-purple-300 
+                        hover:text-purple-700 rounded-md hover:bg-opacity-20 py-5 `} key={i} onClick={() => onClickUserItem(e.key)}><span>{e.icon} </span> <span>{e.key}</span></li>
+                        ))) : (SIGN_IN.map((e, i) => (
+                        <li className={`flex gap-3 mx-5 pl-2 transition-all duration-200 ease-in 
+                        hover:cursor-pointer hover:bg-purple-300 
+                        hover:text-purple-700 rounded-md hover:bg-opacity-20 py-5 `} key={i} onClick={() => onClickUserItem(e.key)}><span>{e.icon} </span> <span>{e.key}</span></li>
+                    ))) }
                 </ul>
-    </div>
+            </div>
         </>
     )
 }
