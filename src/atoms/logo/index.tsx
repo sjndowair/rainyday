e713b746d6478c50043a7f35848e6cb4c93cd326
sjndowair <wodnjsdl1125@naver.com>
2024-12-18 22:21:29 +0900

    feat: logo변경

--- src/atoms/logo/index.tsx
+++ src/atoms/logo/index.tsx
@@ -1,6 +1,6 @@
 import {useNavigate} from "react-router-dom";
 
-import { Umbrella } from "lucide-react";
+import {ChartNoAxesColumnIncreasing, ChartNoAxesColumnDecreasing} from "lucide-react";
 
 
 
@@ -9,9 +9,11 @@ const Logo = () => {
     const navigate = useNavigate();
 
   return (
+
     <h1 onClick={() => navigate("/profile")} className="text-2xl font-bold flex items-center hover:cursor-pointer">
-      <Umbrella className="mr-2" />
-      RainyDaySocial
+      <ChartNoAxesColumnIncreasing className="ml-2 text"/>
+      CharChaoTalk
+        <ChartNoAxesColumnDecreasing className="mr-2" />
     </h1>
   );
 };
