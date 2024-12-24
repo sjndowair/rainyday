8c4bc7b362a9c26140e7c78abc27a6c3fdb120db
sjndowair <wodnjsdl1125@naver.com>
2024-12-24 23:16:09 +0900

    fix: prop 명 변경

--- src/atoms/button/index.tsx
+++ src/atoms/button/index.tsx
@@ -2,13 +2,13 @@ import {IButtonProps} from "../../types/create-membership";
 import {useThemeStore} from "../../store";
 
 
-const Button = ({isLoading, onClickLogOut, children}:IButtonProps) => {
+const Button = ({isLoading, onClick, children}:IButtonProps) => {
 
   const {isDarkMode} = useThemeStore()
 
   return (
     <button
-        onClick={onClickLogOut}
+        onClick={onClick}
       type="submit"
       className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm 
       text-sm font-medium text-white ${isDarkMode ? "bg-blue-600 hover:bg-blue-700" : "bg-purple-500 hover:bg-purple-600 bg-opacity-80"} `}
