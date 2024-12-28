c33fd293e075602d2d64890e1c19e7ad91a1bad8
sjndowair <wodnjsdl1125@naver.com>
2024-12-28 17:10:40 +0900

    fix: userInfo컴포넌트 분리

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -125,18 +125,7 @@ export default function MyPage() {
             </div>
           </div>
         </div>
-        <div
-            className={`${isDarkMode ? "bg-blue-900 " : "bg-purple-300 bg-opacity-10 border border-purple-300 "} bg-opacity-30 rounded-lg p-4 mb-6`}>
-          <h3 className="text-lg font-semibold mb-2">{isUserState?.displayName}님의 한줄 소개글이에요!</h3>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-              <PartyPopper className="h-8 w-8 text-purple-700 opacity-90"/>
-              <p className="bg-transparent border-b border-purple-600 focus:outline-none focus:border-blue-500 transition-colors">
-                {isMessage || "소개글을 입력해주세요."}
-              </p>
-            </div>
-          </div>
-        </div>
+        <UserInfo isUserState={isUserState} isMessage={isMessage}></UserInfo>
         <div className={`flex flex-col gap-10 justify-around`}>
         <MyPageButton value={`게시글 작성하기`} />
         <Post />
