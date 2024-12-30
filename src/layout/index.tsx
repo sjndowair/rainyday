bc8cace30349fcbb666b19812d5a8d1318c8fbc8
sjndowair <wodnjsdl1125@naver.com>
2024-12-30 23:58:35 +0900

    delete: 사용하지않는 레이아웃 삭제

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -20,19 +20,7 @@ const Layout = ({ children }: ILayout) => {
 
   const {toggleTheme,isDarkMode} = useThemeStore();
 
-  //
-  // useEffect(() => {
-  //   const isCreateRainDropEffect = () => {
-  //     const isRainDrop = document.createElement("div");
-  //     isRainDrop.classList.add("rainDrop");
-  //     isRainDrop.style.left = `${Math.random() * 100}%`;
-  //     isRainDrop.style.animationDuration = `${Math.random() * 1 + 0.5}s`;
-  //     document.getElementById("rain-container")?.appendChild(isRainDrop);
-  //     setTimeout(() => isRainDrop.remove(), 2000);
-  //   };
-  //   const isRemoveRainDropEffect = setInterval(isCreateRainDropEffect, 100);
-  //   return () => clearInterval(isRemoveRainDropEffect);
-  // }, []);
+
   return (
            // <Member>
       <>
