c256a0c2498c36138a718cacdabd4794fa7fc514
sjndowair <wodnjsdl1125@naver.com>
2025-01-06 21:13:51 +0900

    fix: button type prop설정 변경

--- src/atoms/button/index.tsx
+++ src/atoms/button/index.tsx
@@ -2,18 +2,20 @@ import {IButtonProps} from "../../types/create-membership";
 import {useThemeStore} from "../../store";
 
 
-const Button = ({isLoading, onClick, children}:IButtonProps) => {
+
+
+const Button = ({isLoading, onClick, children, type}:IButtonProps) => {
 
   const {isDarkMode} = useThemeStore()
 
   return (
     <button
         onClick={onClick}
-      type="submit"
+      type={type}
       className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm 
       text-sm font-medium text-white ${isDarkMode ? "bg-blue-600 hover:bg-blue-700" : "bg-purple-500 hover:bg-purple-600 bg-opacity-80"} `}
     >
-      {isLoading ? "열심히 들어가는중...":children }
+      {isLoading ? "열심히 들어가는중..." : children }
     </button>
   );
 };
