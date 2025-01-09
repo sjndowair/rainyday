e409f4958cf01d8fb2ee3a008a102d6abcec94e8
sjndowair <wodnjsdl1125@naver.com>
2025-01-09 23:25:34 +0900

    fix: 게시글 등록을 위한 postCreationForm 컴포넌트 추가

--- src/components/postCreationForm/index.tsx
+++ src/components/postCreationForm/index.tsx
@@ -1,18 +1,27 @@
 import {useEffect} from "react";
 import InputBox from "../inputBox";
 import Form from "../../atoms/form";
+import Button from "../../atoms/button";
 
 
 interface IPostCreationFormProps {
     onClick?: () => void;
     isOpenModal?: boolean;
+    onSubmit?: (title: string, content: string) => Promise<void>;
+    data?: string
 }
 
-const PostCreationForm = ({onClick, isOpenModal}: IPostCreationFormProps) => {
+const PostCreationForm = ({onClick, isOpenModal, data}: IPostCreationFormProps) => {
+
+
+
+
 
     const onClickInnerBox = (e: React.MouseEvent) => {
         e.stopPropagation()
     }
+
+
 
     useEffect(() => {
         if(isOpenModal) document.body.style.overflow = "hidden";
@@ -21,21 +30,34 @@ const PostCreationForm = ({onClick, isOpenModal}: IPostCreationFormProps) => {
 
 
     return (
-        <div onClick={onClick} className={`bg-purple-500 bg-opacity-50 w-[100%] min-h-screen h-[1300px] absolute top-0 left-0 z-[1] `}>
-        <div
-            onClick={onClickInnerBox}
-            className={`w-[100%] px-[5rem] pt-[4rem] max-w-[900px] h-[40rem] bg-white absolute top-[7rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2"  z-[9999] rounded-2xl`}>
-            <Form>
-                <label>
-                    <h3 className={`text-[1.25rem] text-purple-500 pb-3 font-bold`}>제목</h3>
-                    <InputBox type={`text`}/>
-                </label>
-                <label >
-                    <h3 className={`py-7 text-[1.25rem] mt-3 text-purple-500 font-bold`}>글쓰기</h3>
-                    <textarea className={`bg-purple-100 rounded-xl bg-opacity-90 w-full p-5 h-[15rem]`} />
-                </label>
-            </Form>
-        </div>
+        <div onClick={onClick}
+             className={`bg-purple-500 bg-opacity-50 w-[100%] min-h-screen h-[1300px] absolute top-0 left-0 z-[1] `}>
+            <div
+                onClick={onClickInnerBox}
+                className={`w-[100%] px-[5rem] pt-[4rem] max-w-[900px] h-[40rem] bg-white absolute top-[7rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2"  z-[9999] rounded-2xl`}>
+                <Form >
+                    <label>
+                        <h3 className={`text-[1.25rem] text-purple-500 pb-3 font-bold`}>제목</h3>
+                        <InputBox
+                            type="text"
+
+
+                        />
+                    </label>
+                    <label>
+                        <h3 className={`py-7 text-[1.25rem] mt-3 text-purple-500 font-bold`}>글쓰기</h3>
+                        <textarea
+                            className={`bg-purple-100 rounded-xl bg-opacity-90 w-full p-5 h-[15rem]`}
+
+
+                        />
+                    </label>
+                    <div className={`flex gap-10 pt-10`}>
+                        <Button type="submit">게시</Button>
+                        <Button type="button" onClick={onClick}>취소</Button>
+                    </div>
+                </Form>
+            </div>
         </div>
     )
 }
