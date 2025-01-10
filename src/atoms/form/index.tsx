601a4f140f4efc866f796903487541f8f50a7be5
sjndowair <wodnjsdl1125@naver.com>
2025-01-10 20:22:01 +0900

    fix: form상태 변경

--- src/atoms/form/index.tsx
+++ src/atoms/form/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ILayOutProps } from "../../types/create-membership";
 
 interface IFormProps extends ILayOutProps {
-  isHandleSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+  isHandleSubmit?: (e: React.FormEvent<HTMLFormElement>) => Promise<void> | void | undefined;
 }
 
 const Form = ({ children, isHandleSubmit }: IFormProps) => {
